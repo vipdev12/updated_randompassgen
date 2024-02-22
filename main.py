b77c14719a9b17fdcf2edc806760c23213a11759
Dinislam <aikenamuteshova@gmail.com>
2024-02-22 18:07:18 +0600

    After know about SOLID

--- main.py
+++ main.py
@@ -6,31 +6,40 @@ class RandomPasswordGenerator:
     def __init__(self, pass_length):
         self.pass_length = pass_length
 
-    def generate_numbers(self):
-        res = ''.join(random.choices(string.digits, k=self.pass_length))
-        return res
+    def generate_password(self):
+        pass
 
-    def generate_letters(self):
+
+class RandomLetterPasswordGenerator(RandomPasswordGenerator):
+
+    def generate_password(self):
         res = ''.join(random.choices(string.ascii_letters, k=self.pass_length))
         return res
 
-    def generate_mixed(self):
+
+class RandomMixedPasswordGenerator(RandomPasswordGenerator):
+    def generate_password(self):
         mixed_values = string.ascii_letters + string.digits + string.punctuation
         res = ''.join(random.choices(mixed_values, k=self.pass_length))
+        return res
+
+
+class RandomNumberPasswordGenerator(RandomPasswordGenerator):
+    def generate_password(self):
+        res = ''.join(random.choices(string.digits, k=self.pass_length))
         return res
 
 
 password_type = input('Enter password type (numbers, letters, mixed): ')
 password_length = int(input('Enter password length: '))
 password_quantity = int(input('Enter password quantity: '))
-r = RandomPasswordGenerator(password_length)
 
 for i in range(1, password_quantity+1):
     if password_type == 'numbers':
-        print(r.generate_numbers())
+        print(RandomNumberPasswordGenerator(password_length).generate_password())
     if password_type == 'letters':
-        print(r.generate_letters())
+        print(RandomLetterPasswordGenerator(password_length).generate_password())
     if password_type == 'mixed':
-        print(r.generate_mixed())
+        print(RandomMixedPasswordGenerator(password_length).generate_password())
 
 
